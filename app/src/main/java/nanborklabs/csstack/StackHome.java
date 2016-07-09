23c12e8c30a55b14925a37ffd807d4081db854fc
nanborklabs <nandhu12195@gmail.com>
2016-07-10 01:36:49 +0530

    url updated

---
+++ app/src/main/java/nanborklabs/csstack/StackHome.java
@@ -0,0 +1,304 @@
+package nanborklabs.csstack;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import nanborklabs.csstack.subjects.AIFrag;
+import nanborklabs.csstack.subjects.CGFrag;
+import nanborklabs.csstack.subjects.CNFrag;
+import nanborklabs.csstack.subjects.ComputerarchFragment;
+import nanborklabs.csstack.subjects.CplusFragment;
+import nanborklabs.csstack.subjects.DaaFrag;
+import nanborklabs.csstack.subjects.DatastructresFrag;
+import nanborklabs.csstack.subjects.DbmsFrag;
+import nanborklabs.csstack.subjects.FlatFrag;
+import nanborklabs.csstack.subjects.JavaFrag;
+import nanborklabs.csstack.subjects.MPFrag;
+import nanborklabs.csstack.subjects.MobileFrag;
+import nanborklabs.csstack.subjects.OSFrag;
+import nanborklabs.csstack.subjects.OoadFrag;
+import nanborklabs.csstack.subjects.PCDFrag;
+import nanborklabs.csstack.subjects.POMFrag;
+import nanborklabs.csstack.subjects.PPFrag;
+import nanborklabs.csstack.subjects.Parallel;
+import nanborklabs.csstack.subjects.SEFrag;
+import nanborklabs.csstack.subjects.SSFrag;
+import nanborklabs.csstack.subjects.SecurityFrag;
+import nanborklabs.csstack.subjects.computingtechniqueFragment;
+import nanborklabs.csstack.subjects.prinicplesofCEFragment;
+
+public class StackHome extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    NavigationView left_side;
+    NavigationView right_side;
+    boolean right_click;
+    boolean left_click;
+    Fragment subjectFragment;
+    public int year_selected;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stack_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        left_side= (NavigationView) findViewById(R.id.nav_view);
+        right_side=(NavigationView)findViewById(R.id.right_side);
+        left_side.setNavigationItemSelectedListener(this);
+        right_side.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.stack_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+
+            case R.id.first:
+                // Handle the camera action
+                left_click = true;
+                right_side.getMenu().clear();
+                right_side.inflateMenu(R.menu.first_year);
+                year_selected=1;
+                break;
+
+
+            case R.id.second:
+                left_click = true;
+                right_side.getMenu().clear();
+                right_side.inflateMenu(R.menu.second_year);
+                year_selected=2;
+                break;
+            case  R.id.third:
+                left_click = true;
+                right_side.getMenu().clear();
+                right_side.inflateMenu(R.menu.third_year);
+                year_selected=3;
+                break;
+            case  R.id.fourth:
+                left_click = true;
+                right_side.getMenu().clear();
+                right_side.inflateMenu(R.menu.fourth_year);
+                year_selected=4;
+                break;
+            case R.id.nav_share:
+                left_click = true;
+                break;
+
+            case  R.id.nav_send:
+                left_click = true;
+                break;
+
+
+            //Right side click events
+
+            case R.id.cp:
+                right_click = true;
+                Log.d("COM_CSTACK", "CP CLICKED");
+                subjectFragment = new computingtechniqueFragment();
+                break;
+
+            case  R.id.pcp:
+                right_click = true;
+                subjectFragment = new prinicplesofCEFragment();
+                break;
+            case  R.id.cplus:
+                right_click = true;
+                subjectFragment = new CplusFragment();
+                break;
+            case R.id.ca:
+                right_click = true;
+                subjectFragment = new ComputerarchFragment();
+                break;
+            case  R.id.ds:
+                right_click = true;
+                subjectFragment = new DatastructresFrag();
+                break;
+            case R.id.dbms:
+                right_click = true;
+                subjectFragment = new DbmsFrag();
+                break;
+            case R.id.daa:
+                right_click = true;
+                subjectFragment = new DaaFrag();
+            case R.id.jip:
+                right_click = true;
+                subjectFragment = new JavaFrag();
+                break;
+            case  R.id.os:
+                right_click = true;
+                subjectFragment = new OSFrag();
+                break;
+            case R.id.se:
+                right_click = true;
+                subjectFragment = new SEFrag();
+                break;
+            case R.id.cn:
+                right_click = true;
+                subjectFragment = new CNFrag();
+                break;
+            case  R.id.mp:
+                right_click = true;
+                subjectFragment = new MPFrag();
+                break;
+            case  R.id.ss:
+                right_click = true;
+                subjectFragment = new SSFrag();
+                break;
+            case R.id.flat:
+                right_click = true;
+                subjectFragment = new FlatFrag();
+                break;
+            case  R.id.ooad:
+                right_click = true;
+                subjectFragment = new OoadFrag();
+                break;
+            case R.id.ai:
+                right_click = true;
+                subjectFragment = new AIFrag();
+                break;
+            case R.id.pcd:
+                right_click = true;
+
+                subjectFragment = new PCDFrag();
+                break;
+            case  R.id.cg:
+                right_click = true;
+                subjectFragment = new CGFrag();
+                break;
+            case  R.id.paradigms:
+                right_click = true;
+                subjectFragment = new PPFrag();
+                break;
+            case  R.id.pom:
+                right_click = true;
+                subjectFragment = new POMFrag();
+                break;
+            case  R.id.mobile:
+                right_click = true;
+                subjectFragment = new MobileFrag();
+                break;
+
+            case R.id.parallel:
+                right_click = true;
+                subjectFragment = new Parallel();
+                break;
+            case R.id.security:
+                right_click = true;
+                subjectFragment = new SecurityFrag();
+                break;
+            default:
+                return true;
+        }
+
+
+        if (right_click) {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            Log.d("COM_CSTACK", "inside if");
+            drawer.closeDrawer(GravityCompat.END);
+            //       subjectFragment=new TestFragment();
+            Log.d("CS_STACK","GOING TO trasact fragment");
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frag_holder, subjectFragment)
+                    .commit();
+            right_click=false;
+
+        }
+
+
+        if (left_click) {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            drawer.openDrawer(GravityCompat.END);
+            left_click = false;
+        }
+
+
+        return true;
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("year_selected",year_selected);
+        Log.d("CS_STACK","Save instance state---avtivity");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("CS_STACK","on Resotre Instance state--------activity");
+        int year=savedInstanceState.getInt("year_selected");
+        Log.d("CS_STACK------>",""+year);
+        if (year==0){
+            year=1;
+        }
+        switch (year){
+            case 1:
+                right_side.inflateMenu(R.menu.first_year);
+                break;
+            case 2:
+                right_side.inflateMenu(R.menu.second_year);
+                break;
+            case 3:
+                right_side.inflateMenu(R.menu.third_year);
+                break;
+            case 4:
+                right_side.inflateMenu(R.menu.fourth_year);
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/nanborklabs/csstack/subjects/SEFrag.java
@@ -0,0 +1,197 @@
+package nanborklabs.csstack.subjects;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nanborklabs.csstack.Points.AIpoints;
+import nanborklabs.csstack.Points.CGpoints;
+import nanborklabs.csstack.Points.SEpoints;
+import nanborklabs.csstack.R;
+
+/**
+ * Created by nandhu on 8/7/16.
+ */
+public class SEFrag extends android.support.v4.app.Fragment {
+    public View mView;
+    ViewPager mViewPager;
+    public PagerAdapter mAdapter;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d("CS_STACK","on attach view pager");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d("CS_STACK","on detach VIEW PAGER");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("CS_STACK","on destroy VIEW PAGER");
+        mAdapter=null;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("CS_STACK","on destroy view View pager");
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("CS_STACK","on CREATE====in view pager fragment");
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView=inflater.inflate(R.layout.viewpager,container,false);
+        mViewPager=(ViewPager)mView.findViewById(R.id.pager);
+        mAdapter=new ViewPagerAdapter(getChildFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+        mViewPager.setOffscreenPageLimit(3);
+
+        TabLayout tabLayout=(TabLayout)mView.findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        Log.d("CS_STACK","on CREATE====in view pager fragment");
+        return mView;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("CS_STACK","on start====in view pager fragment");
+        mViewPager.setCurrentItem(0);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d("CS_STACK","on view created====in view pager fragment");
+    }
+
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        public SEpoints showpointsFragment;
+
+
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+            Log.d("CS_STACK","VIEW PAGER");
+
+            if (showpointsFragment==null){
+                showpointsFragment=new SEpoints();
+            }
+            else {
+                Log.d("CS_STACK","show points fragment is not null");
+            }
+
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position) {
+                case 0:
+
+                    return "UNIT I";
+
+                case 1:
+                    return "UNIT II";
+
+                case 2:
+                    return "UNIT III";
+
+                case 3:
+                    return "UNIT IV";
+
+                case 4:
+                    return "UNIT V";
+
+
+            }
+
+            return null;
+
+
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Log.d("CS_STACK","fragment returning from view pager");
+            switch (position){
+                case 0:
+                    return  SEpoints.newInstance(position);
+
+                case 1:
+                    return  SEpoints.newInstance(position);
+                case 2:
+                    return  SEpoints.newInstance(position);
+                case 3:
+                    return  SEpoints.newInstance(position);
+                case 4:
+                    return  SEpoints.newInstance(position);
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            super.destroyItem(container, position, object);
+            Log.d("CS_STACK","view pager destroy item");
+
+
+
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Log.d("CS_STACK","intantiate item view pager");
+
+            return super.instantiateItem(container, position);
+
+        }
+    }
+
+
+}
