960ac78d93c4085f741682e14a547ecd8cf50a94
nanborklabs <nandhu12195@gmail.com>
2016-07-09 16:02:56 +0530

    first proper

--- app/src/main/java/nanborklabs/csstack/adapter/rv_adapter.java
+++ app/src/main/java/nanborklabs/csstack/adapter/rv_adapter.java
@@ -1,7 +1,74 @@
 package nanborklabs.csstack.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import nanborklabs.csstack.R;
+
 /**
  * Created by nandhu on 9/7/16.
  */
-public class rv_adapter {
+public class rv_adapter extends RecyclerView.Adapter<rv_adapter.ItemHolder> {
+
+
+    public ArrayList<String> point;
+    public Point_clicked callback;
+    public View mView;
+
+    public rv_adapter(ArrayList<String> points,Point_clicked callback) {
+        this.callback = callback;
+        this.point=points;
+    }
+
+    public static class ItemHolder extends RecyclerView.ViewHolder{
+        public TextView mTextView;
+        rv_adapter.Point_clicked callback;
+
+        public ItemHolder(View itemView, Point_clicked callback1) {
+            super(itemView);
+            mTextView=(TextView) itemView;
+            this.callback=callback1;
+            mTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (callback !=null){
+                        callback.point_clicked(getAdapterPosition());
+                    }
+
+                }
+            });
+        }
+    }
+    @Override
+    public rv_adapter.ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+       mView= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_points,parent,false);
+        if (callback==null){
+            Log.d("CS_STACK","callback is null");
+        }
+        return new ItemHolder(mView,callback);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemHolder holder, int position) {
+        holder.mTextView.setText(point.get(position));
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return point.size();
+    }
+
+
+    public interface Point_clicked{
+        void point_clicked(int position);
+    }
 }
--- app/src/main/java/nanborklabs/csstack/subjects/ViewPagerFragment.java
+++ app/src/main/java/nanborklabs/csstack/subjects/ViewPagerFragment.java
@@ -1,7 +1,62 @@
 package nanborklabs.csstack.subjects;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nanborklabs.csstack.R;
+
 /**
  * Created by nandhu on 8/7/16.
  */
-public class ViewPagerFragment {
+public class ViewPagerFragment extends Fragment {
+    public View mView;
+    RecyclerView mList;
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+     mView=inflater.inflate(R.layout.points_showing,container,false);
+        mList=(RecyclerView)mView.findViewById(R.id.points_list);
+
+        return mView;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
